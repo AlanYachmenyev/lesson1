218f12e27604126e08e5f14deda49ea0971b7504
AlanYachmenyev <nhmurder@mail.ru>
2024-09-06 16:49:12 +0300

    create file lesson1

--- les1.py
+++ les1.py
@@ -1,1 +1,21 @@
-ghjjk
+per1 = 1
+print(per1)
+per2 = 12
+print(per2)
+per3 = 34.948
+print(per3)
+per4 = 0.0495
+print(per4)
+per5 = "hi"
+print(per5)
+per6 = "Hello World"
+print(per6)
+per7 = False
+print(per7)
+per8 = True
+print(per8)
+per9 = None
+print(None)
+name = "Alan"
+print("Full name:","Alan",'Yachmenev')
+print("Hi!",name)
